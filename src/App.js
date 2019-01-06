b2883bd941d3b76687c4e45ff48dc88f8e2b9c99
vamsi <vamsi@quintype.com>
2019-01-06 11:20:44 +0530

    Initial commit of todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Todo extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  
+  render() {
+    return(
+      <div>
+        {this.props.todo}
+        <span onClick={() => this.props.onRemove(this.props.todo)}> remove </span>
+      </div>
+    )
+  }
+}
+
+
+class InputComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "enter your input"
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <input value={this.state.value} onChange={(e) => this.setState({value: e.target.value})} />
+        <input type="submit" onClick={() => this.props.onSubmit(this.state.value)} />
+      </div>
+    )
+  }
+
+         
+}
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: ["wash", "cook", "salon"]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <InputComponent onSubmit={(newItem) => this.setState({todos: this.state.todos.concat(newItem)})}/>
+        { this.state.todos.map(todo => <Todo todo={todo} onRemove={(value) => this.setState({todos: this.state.todos.filter((item) => item !== value)})} />) }
       </div>
     );
   }
